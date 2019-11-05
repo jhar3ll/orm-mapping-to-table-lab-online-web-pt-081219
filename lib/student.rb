0c341599eabe1b0cdea3ac607a8d059d9e257679
Jharell Chambers <jharell.chambers@selu.edu>
2019-11-05 18:11:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -30,6 +30,6 @@ end
     SQL
     
     DB[:conn].execute(sql, self.name, self.album)
- @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
   
   
