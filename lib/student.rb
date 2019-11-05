5b6e029c9653f21de7573c2fb9c4231024e7d8dc
Jharell Chambers <jharell.chambers@selu.edu>
2019-11-05 18:02:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,13 @@
 class Student
-  attr_accessor :name, :grade, :id 
+  attr_accessor :name, :grade 
+  attr_reader   :id 
   
   def initialize (name, grade, id=nil)
     @name = name 
     @grade = grade 
     @id = id
-  
 end
+
+  def self.create_table
+    sql = <<- 
+  end 
